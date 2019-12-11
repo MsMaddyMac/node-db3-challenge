190d4d107528301a3487965e2abed27abe368f7e
Maddy <mmcintosh2k14@gmail.com>
2019-12-11 16:45:33 -0500

    created findSteps helper method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,7 +2,8 @@ const db = require('../data/db-config');
 
 module.exports = {
     find,
-    findById
+    findById,
+    findSteps
 }
 
 function find() {
@@ -13,4 +14,11 @@ function findById(id) {
     return db('schemes')
     .where({ id })
     .first();
+}
+
+function findSteps(id) {
+    return db('steps as st')
+    .select('st.id', 's.scheme_name', 'st.step_number', 'st.instructions')
+    .join('schemes as s', 'st.scheme_id', 's.id')
+    .where({ scheme_id: id });
 }
