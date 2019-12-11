eb5fc3d51e4e226cb934a0cd7f83b3770476e4a5
Maddy <mmcintosh2k14@gmail.com>
2019-12-11 16:10:00 -0500

    adds SQL statements to queries.sql

--- queries.sql
+++ queries.sql
@@ -7,8 +7,26 @@ from [Product] as p
 join category as c on p.CategoryId = c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select o.id
+    , s.CompanyName
+from [Order] as o
+join Shipper as s on o.ShipVia = s.id
+where OrderDate < "2012-08-09";
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select p.ProductName
+, od.Quantity
+from [Order] as o
+join Product as p on od.ProductId = p.id
+join OrderDetail as od on o.id = od.OrderId 
+where o.id = "10251"
+order by p.ProductName asc;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+select o.id as OrderID
+, c.CompanyName as Customer
+, e.lastName  as Employee
+from [Order] as o
+join Customer as c on o.CustomerId = c.id
+join Employee as e on o.EmployeeId = e.id;
